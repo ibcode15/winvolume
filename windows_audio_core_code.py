34c13e13f36bbf896c9ac38c6821557bcb14469e
isaac butchard <59832706+ibcode15@users.noreply.github.com>
2020-12-10 22:27:59 +0000

    Delete windows_audio_core_code.py

--- windows_audio_core_code.py
+++
@@ -1,6 +0,0 @@
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-
-def windows_audio_core():
-    return cast(AudioUtilities.GetSpeakers().Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None), POINTER(IAudioEndpointVolume))
